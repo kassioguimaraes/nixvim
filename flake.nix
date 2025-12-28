07070ace73cb41ff2321250237133d6afc05bd43
Kassio Guimaraes <kassio@tuta.com>
2025-12-28 14:38:57 -0300

    allow unfree

--- flake.nix
+++ flake.nix
@@ -1,49 +1,44 @@
 {
   description = "A nixvim configuration";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixvim.url = "github:nix-community/nixvim";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
-
-  outputs = {
-    nixvim,
-    flake-parts,
-    ...
-  } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs = { nixvim, flake-parts, nixpkgs, ... } @ inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
         "aarch64-linux"
         "x86_64-darwin"
         "aarch64-darwin"
       ];
+      perSystem = { system, ... }: let
+        # Import nixpkgs with unfree allowed
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;  # Allows all unfree
 
-      perSystem = {
-        pkgs,
-        system,
-        ...
-      }: let
+          # For finer control (recommended - only allow specific packages):
+          # config.allowUnfreePredicate = pkg:
+          #   builtins.elem (pkgs.lib.getName pkg) [
+          #     "intelephense"
+          #   ];
+        };
+
         nixvimLib = nixvim.lib.${system};
         nixvim' = nixvim.legacyPackages.${system};
         nixvimModule = {
           inherit pkgs;
-          module = import ./config; # import the module directly
-          # You can use `extraSpecialArgs` to pass additional arguments to your module files
-          extraSpecialArgs = {
-            # inherit (inputs) foo;
-          };
+          module = import ./config;
+          extraSpecialArgs = { };
         };
         nvim = nixvim'.makeNixvimWithModule nixvimModule;
       in {
         checks = {
-          # Run `nix flake check .` to verify that your config is not broken
           default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
         };
-
         packages = {
-          # Lets you run `nix run .` to start nixvim
           default = nvim;
         };
       };
